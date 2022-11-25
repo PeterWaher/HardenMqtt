0400b637157b72c79b63caa63b3091aabe1a6dcd
Peter Waher <peterwaher@hotmail.com>
2022-11-25 14:31:11 -0300

    Adding projects

---
+++ Display/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Display
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ Sensor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sensor
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
