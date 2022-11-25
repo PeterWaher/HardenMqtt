f903200ca84cc6c5810471b7d897143be607cc28
Peter Waher <peterwaher@hotmail.com>
2022-11-25 17:18:19 -0300

    Configuring and connecting to broker

--- Sensor/Program.cs
+++ Sensor/Program.cs
@@ -1,12 +1,248 @@
 ﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Waher.Events;
+using Waher.Events.Console;
+using Waher.Networking.MQTT;
+using Waher.Persistence;
+using Waher.Persistence.Files;
+using Waher.Runtime.Inventory;
+using Waher.Runtime.Inventory.Loader;
+using Waher.Runtime.Settings;
 
 namespace Sensor
 {
+	/// <summary>
+	/// This project implements a simple sensor that gets its values from the Internet and publishes it on an MQTT Broker. 
+	/// It publishes the information in four different ways: Unsecure and Unstructured, Unsecure and Structured, 
+	/// Secured but public, Secured and Confidential.
+	/// </summary>
 	internal class Program
 	{
-		static void Main(string[] args)
+		/// <summary>
+		/// Program entry point
+		/// </summary>
+		static async Task Main()
 		{
-			Console.WriteLine("Hello World!");
+			FilesProvider DBProvider = null;
+			MqttClient mqtt = null;
+
+			try
+			{
+				// First, initialize environment and type inventory. This creates an inventory of types used by the application.
+				// This is important for tasks such as data persistence, for example.
+				TypesLoader.Initialize();
+
+				// Setup Event Output to Console window
+				Log.Register(new ConsoleEventSink());
+				Log.Informational("Sensor application starting...");
+
+				// Setup database
+				Log.Informational("Setting up database...");
+				DBProvider = await FilesProvider.CreateAsync("Database", "Default", 8192, 10000, 8192, Encoding.UTF8, 10000, true, false);
+				Database.Register(DBProvider);
+
+				// Repair database, if an inproper shutdown is detected
+				await DBProvider.RepairIfInproperShutdown(string.Empty);
+
+				// Starting internal modules
+				await Types.StartAllModules(60000);
+
+				// Configuring and connecting to MQTT Server
+
+				bool MqttConnected = false;
+
+				do
+				{
+					string MqttHost = await RuntimeSettings.GetAsync("MQTT.Host", string.Empty);
+					int MqttPort = (int)await RuntimeSettings.GetAsync("MQTT.Port", 8883);
+					bool MqttEncrypted = await RuntimeSettings.GetAsync("MQTT.Tls", true);
+					string MqttUserName = await RuntimeSettings.GetAsync("MQTT.UserName", string.Empty);
+					string MqttPassword = await RuntimeSettings.GetAsync("MQTT.Password", string.Empty);
+
+					if (string.IsNullOrEmpty(MqttHost))
+					{
+						Console.Out.WriteLine("MQTT host not configured. Please provide the connection details below. If the default value presented is sufficient, just press ENTER.");
+
+						MqttHost = UserInput("MQTT Host", MqttHost);
+						await RuntimeSettings.SetAsync("MQTT.Host", MqttHost);
+
+						MqttPort = UserInput("MQTT Port", MqttPort, 1, 65535);
+						await RuntimeSettings.SetAsync("MQTT.Port", MqttPort);
+
+						MqttEncrypted = (MqttPort == 8883);
+						MqttEncrypted = UserInput("Encrypt with TLS", MqttEncrypted);
+						await RuntimeSettings.SetAsync("MQTT.Tls", MqttEncrypted);
+
+						MqttUserName = UserInput("MQTT UserName", MqttUserName);
+						await RuntimeSettings.SetAsync("MQTT.UserName", MqttUserName);
+
+						MqttPassword = UserInput("MQTT Password", MqttPassword);
+						await RuntimeSettings.SetAsync("MQTT.Password", MqttPassword);
+					}
+
+					if (!(mqtt is null))
+					{
+						await mqtt.DisposeAsync();
+						mqtt = null;
+					}
+
+					Log.Informational("Connecting to MQTT Broker...");
+
+					TaskCompletionSource<bool> WaitForConnect = new TaskCompletionSource<bool>();
+
+					mqtt = new MqttClient(MqttHost, MqttPort, MqttEncrypted, MqttUserName, MqttPassword);
+
+					mqtt.OnConnectionError += (_, e) =>
+					{
+						Log.Error(e.Message);
+						WaitForConnect.TrySetResult(false);
+						return Task.CompletedTask;
+					};
+
+					mqtt.OnError += (_, e) =>
+					{
+						Log.Error(e.Message);
+						return Task.CompletedTask;
+					};
+
+					mqtt.OnStateChanged += (_, NewState) =>
+					{
+						Log.Informational(NewState.ToString());
+
+						switch (NewState)
+						{
+							case MqttState.Offline:
+							case MqttState.Error:
+								WaitForConnect.TrySetResult(false);
+								break;
+
+							case MqttState.Connected:
+								WaitForConnect.TrySetResult(true);
+								break;
+						}
+
+						return Task.CompletedTask;
+					};
+
+					MqttConnected = await WaitForConnect.Task;
+				}
+				while (!MqttConnected);
+
+				// Configure CTRL+Z to close application gracefully.
+
+				bool Continue = true;
+
+				Console.CancelKeyPress += (_, e) =>
+				{
+					e.Cancel = true;
+					Continue = false;
+				};
+
+				// Normal operation
+
+				Log.Informational("Sensor application started... Press CTRL+C to terminate the application.");
+
+				while (Continue)
+					await Task.Delay(100);
+			}
+			catch (Exception ex)
+			{
+				// Display exception terminating application
+				Log.Alert(ex);
+			}
+			finally
+			{
+				// Shut down database gracefully
+
+				Log.Informational("Sensor application stopping...");
+
+				if (!(mqtt is null))
+				{
+					await mqtt.DisposeAsync();
+					mqtt = null;
+				}
+
+				if (!(DBProvider is null))
+					await DBProvider.Flush();
+
+				await Types.StopAllModules();
+				Log.Terminate();
+			}
 		}
+
+		#region UserInput methods
+
+		/// <summary>
+		/// Asks the user to input a string.
+		/// </summary>
+		/// <param name="Label">Label</param>
+		/// <param name="Default">Default value</param>
+		/// <returns>User input</returns>
+		private static string UserInput(string Label, string Default)
+		{
+			Console.Out.Write(Label);
+
+			if (!string.IsNullOrEmpty(Default))
+			{
+				Console.Out.Write(" (default: ");
+				Console.Out.Write(Default);
+				Console.Out.Write(")");
+			}
+
+			Console.Out.Write(": ");
+
+			string s = Console.In.ReadLine();
+
+			if (string.IsNullOrEmpty(s))
+				return Default;
+			else
+				return s;
+		}
+
+		/// <summary>
+		/// Asks the user to input a Boolean value.
+		/// </summary>
+		/// <param name="Label">Label</param>
+		/// <param name="Default">Default value</param>
+		/// <returns>User input</returns>
+		private static bool UserInput(string Label, bool Default)
+		{
+			string s;
+			bool Result;
+
+			do
+			{
+				s = UserInput(Label, Default.ToString());
+			}
+			while (!bool.TryParse(s, out Result));
+
+			return Result;
+		}
+
+		/// <summary>
+		/// Asks the user to input a 32-bit integer.
+		/// </summary>
+		/// <param name="Label">Label</param>
+		/// <param name="Default">Default value</param>
+		/// <param name="Min">Smallest permitted value.</param>
+		/// <param name="Max">Largest permitted value.</param>
+		/// <returns>User input</returns>
+		private static int UserInput(string Label, int Default, int Min, int Max)
+		{
+			string s;
+			int Result;
+
+			do
+			{
+				s = UserInput(Label, Default.ToString());
+			}
+			while (!int.TryParse(s, out Result) || Result < Min || Result > Max);
+
+			return Result;
+		}
+
+		#endregion
+
 	}
 }
