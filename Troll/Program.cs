758e2869c36421e4c559df7043043c65e07be087
Peter Waher <peterwaher@hotmail.com>
2022-11-29 20:10:02 -0300

    Trolling

--- Troll/Program.cs
+++ Troll/Program.cs
@@ -12,6 +12,13 @@ using Waher.Runtime.Settings;
 using System.Threading;
 using Waher.Events.MQTT;
 using Waher.Runtime.Queue;
+using Waher.Content;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Xml;
+using Waher.Content.Xml;
+using Waher.Runtime.Cache;
+using Waher.Security;
 
 namespace Troll
 {
@@ -214,6 +221,51 @@ namespace Troll
 					MqttContent e = await InputQueue.Wait();
 					if (e is null)
 						continue;
+
+					try
+					{
+						string Digest = Hashes.ComputeSHA256HashString(e.Data);
+						if (recentlySent.ContainsKey(Digest))
+							continue;
+
+						if (e.Data.Length > 65536)
+							await TrollBlob(Mqtt, e.Topic, e.Data);
+						else
+						{
+							string s = Encoding.UTF8.GetString(e.Data);
+
+							if (long.TryParse(s, out long i))
+								await TrollInteger(Mqtt, e.Topic, i);
+							else if (CommonTypes.TryParse(s, out double d))
+								await TrollDouble(Mqtt, e.Topic, d);
+							else if (TimeSpan.TryParse(s, out TimeSpan TS))
+								await TrollTimeSpan(Mqtt, e.Topic, TS);
+							else if (DateTime.TryParse(s, out DateTime TP))
+								await TrollDateTime(Mqtt, e.Topic, TP);
+							else if (DateTimeOffset.TryParse(s, out DateTimeOffset TPO))
+								await TrollDateTimeOffset(Mqtt, e.Topic, TPO);
+							else if (CommonTypes.TryParseRfc822(s, out TPO))
+								await TrollDateTimeOffset(Mqtt, e.Topic, TPO);
+							else if (XML.TryParse(s, out TP))
+								await TrollDateTime(Mqtt, e.Topic, TP);
+							else if (XML.TryParse(s, out TPO))
+								await TrollDateTimeOffset(Mqtt, e.Topic, TPO);
+							else if (Uri.TryCreate(s, UriKind.Absolute, out Uri Link))
+								await TrollUri(Mqtt, e.Topic, Link);
+							else if (TryParseJsonObject(s, out Dictionary<string, object> Object))
+								await TrollObject(Mqtt, e.Topic, Object);
+							else if (TryParseJsonArray(s, out Array Vector))
+								await TrollArray(Mqtt, e.Topic, Vector);
+							else if (TryParseXml(s, out XmlDocument Xml))
+								await TrollXml(Mqtt, e.Topic, Xml);
+							else
+								await TrollString(Mqtt, e.Topic, s);
+						}
+					}
+					catch (Exception ex)
+					{
+						Log.Critical(ex, DeviceID);
+					}
 				}
 
 				#endregion
@@ -313,6 +365,187 @@ namespace Troll
 
 			return Result;
 		}
+
+		#endregion
+
+		#region Parsing Methods
+
+		/// <summary>
+		/// Tries to parse a string as a JSON object.
+		/// </summary>
+		/// <param name="s">String</param>
+		/// <param name="Result">Parsed Object</param>
+		/// <returns>If object could be parsed.</returns>
+		private static bool TryParseJsonObject(string s, out Dictionary<string, object> Result)
+		{
+			Result = null;
+			s = s.Trim();
+			if (!s.StartsWith("{") || !s.EndsWith("}"))
+				return false;
+
+			try
+			{
+				object Parsed = JSON.Parse(s);
+				Result = Parsed as Dictionary<string, object>;
+				return !(Result is null);
+			}
+			catch
+			{
+				Result = null;
+				return false;
+			}
+		}
+
+		/// <summary>
+		/// Tries to parse a string as a JSON array.
+		/// </summary>
+		/// <param name="s">String</param>
+		/// <param name="Result">Parsed array</param>
+		/// <returns>If array could be parsed.</returns>
+		private static bool TryParseJsonArray(string s, out Array Result)
+		{
+			Result = null;
+			s = s.Trim();
+			if (!s.StartsWith("[") || !s.EndsWith("]"))
+				return false;
+
+			try
+			{
+				object Parsed = JSON.Parse(s);
+				Result = Parsed as Array;
+				return !(Result is null);
+			}
+			catch
+			{
+				Result = null;
+				return false;
+			}
+		}
+
+		/// <summary>
+		/// Tries to parse a string as an XML document.
+		/// </summary>
+		/// <param name="s">String</param>
+		/// <param name="Result">Parsed XML document</param>
+		/// <returns>If document could be parsed.</returns>
+		private static bool TryParseXml(string s, out XmlDocument Result)
+		{
+			Result = null;
+			s = s.Trim();
+			if (!s.StartsWith("<") || !s.EndsWith(">"))
+				return false;
+
+			try
+			{
+				XmlDocument Doc = new XmlDocument();
+				Doc.LoadXml(s);
+
+				Result = Doc;
+				return true;
+			}
+			catch
+			{
+				Result = null;
+				return false;
+			}
+		}
+
+		#endregion
+
+		#region Trolling
+
+		private static async Task TrollBlob(MqttClient Mqtt, string Topic, byte[] Data)
+		{
+			int c = Data.Length;
+
+			switch (rnd.Next(6))
+			{
+				case 0:	// Half size
+					Array.Resize<byte>(ref Data, c / 2);
+					await Publish(Mqtt, Topic, Data);
+					break;
+
+				case 1: // Double size
+					Array.Resize<byte>(ref Data, c * 2);
+					Array.Copy(Data, 0, Data, c, c);
+					await Publish(Mqtt, Topic, Data);
+					break;
+
+				case 2: // Randomize
+					rnd.NextBytes(Data);
+					await Publish(Mqtt, Topic, Data);
+					break;
+
+				case 3: // 1 MB
+					Data = new byte[1024 * 1024];
+					rnd.NextBytes(Data);
+					await Publish(Mqtt, Topic, Data);
+					break;
+
+				case 4: // 16 MB
+					Data = new byte[16 * 1024 * 1024];
+					rnd.NextBytes(Data);
+					await Publish(Mqtt, Topic, Data);
+					break;
+
+				case 5: // 192 MB
+					Data = new byte[192 * 1024 * 1024];
+					rnd.NextBytes(Data);
+					await Publish(Mqtt, Topic, Data);
+					break;
+			}
+		}
+
+		private static async Task TrollInteger(MqttClient Mqtt, string Topic, long i)
+		{
+		}
+
+		private static async Task TrollDouble(MqttClient Mqtt, string Topic, double d)
+		{
+		}
+
+		private static async Task TrollTimeSpan(MqttClient Mqtt, string Topic, TimeSpan TS)
+		{
+		}
+
+		private static async Task TrollDateTime(MqttClient Mqtt, string Topic, DateTime TP)
+		{
+		}
+
+		private static async Task TrollDateTimeOffset(MqttClient Mqtt, string Topic, DateTimeOffset TPO)
+		{
+		}
+
+		private static async Task TrollUri(MqttClient Mqtt, string Topic, Uri Link)
+		{
+		}
+
+		private static async Task TrollString(MqttClient Mqtt, string Topic, string s)
+		{
+		}
+
+		private static async Task TrollObject(MqttClient Mqtt, string Topic, Dictionary<string, object> Object)
+		{
+		}
+
+		private static async Task TrollArray(MqttClient Mqtt, string Topic, Array Array)
+		{
+		}
+
+		private static async Task TrollXml(MqttClient Mqtt, string Topic, XmlDocument Xml)
+		{
+		}
+
+		private static async Task Publish(MqttClient Mqtt, string Topic, byte[] Data)
+		{
+			string Digest = Hashes.ComputeSHA256HashString(Data);
+			recentlySent[Digest] = true;
+
+			await Mqtt.PUBLISH(Topic, MqttQualityOfService.AtMostOnce, true, Data);
+		}
+
+		private static readonly Random rnd = new Random();
+		private static readonly Cache<string, bool> recentlySent = new Cache<string, bool>(int.MaxValue, TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(1));
 
 		#endregion
 
